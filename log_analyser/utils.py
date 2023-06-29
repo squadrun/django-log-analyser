9f875f42bc3e43bd9dd23825fb16d0720c69031c
Sandal Jain <sandal.jain@squadiq.in>
2023-06-29 20:17:09 +0530

    removing server user

--- log_analyser/utils.py
+++ log_analyser/utils.py
@@ -18,8 +18,6 @@ from .models import DBLogReportDetail
 logger = logging.getLogger(__name__)
 
 aws_session = Session(
-    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
     region_name=settings.AWS_REGION
 )
 rds_client = aws_session.client('rds')
